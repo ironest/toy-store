fd11295270f99f05043f6f4073526f4a736381fd
ironest <ironest@gmail.com>
2019-10-14 01:40:25 +1100

    Added Seed file to pre-populate data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+for i in 1..20
+
+    entry = {
+        name: Faker::Game.title,
+        description: Faker::Lorem.paragraph,
+        posted: Faker::Date.between(from: 1.year.ago, to: 2.days.ago),
+        user: Faker::Name.first_name
+    }
+
+    Toy.create( entry )
+
+    puts "Toy #{i} | #{entry[:name]} | #{entry[:description]} | #{entry[:posted]} | #{entry[:user]}"
+
+end
