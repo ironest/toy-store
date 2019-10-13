cdb55803183246efb66bf6ec729d72f735087dd1
ironest <ironest@gmail.com>
2019-10-14 01:49:18 +1100

    Added CRUD routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/toys", to: "toys#index", as: "toys"
+  get "/toys/new", to: "toys#new", as: "new_toy"
+  post "/toys", to: "toys#create"
+  get "/toys/:id", to: "toys#show", as: "toy"
+  get "/toys/:id/edit", to: "toys#edit", as: "edit_toy"
+  put "/toys", to: "toys#update"
+  patch "/toys", to: "toys#update"
+  delete "/toys/:id", to: "toys#destroy"  
+
 end
